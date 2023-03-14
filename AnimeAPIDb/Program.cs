209ae54bb0148020274f51bc33181cd12a6febf8
Ivan Kosmin <37915584+ICHTLAY@users.noreply.github.com>
2023-03-14 23:15:14 +0200

    Add ASP.NET Web App template files

--- AnimeAPIDb/Program.cs
+++ AnimeAPIDb/Program.cs
@@ -1,5 +1,5 @@
-using System.Text.Json.Serialization;
 using AnimeAPIDb;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,22 +10,36 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContextPool<AnimeContext>(options =>
+builder.Services.AddDbContext<AnimeContext>(options =>
     options.UseSqlite("Filename=MyDatabase.db"));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AnimeContext>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseMigrationsEndPoint();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseRouting();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllers();
 
 app.Run();
